60e255655ba0329aa838fa9e4902695aef84e37e
Mohammed Mishal M <mishalsonu2003@gmail.com>
2025-11-24 19:56:04 +0530

    book details finished

--- q19.php
+++ q19.php
@@ -7,6 +7,7 @@ $database = "Mishal_db";
 
 $conn = new mysqli($servername, $username, $password, $database);
 
+
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
